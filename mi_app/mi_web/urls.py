6d4b5cdcd8f02f0a0a73b3be7b308f07ca560bde
Christian-Coria <desbloqueoenlinea@gmail.com>
2022-07-10 12:04:29 -0300

    cargando html de forms, urls,y definiendo funciones

--- mi_app/mi_web/urls.py
+++ mi_app/mi_web/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from mi_web.views import registrando_cliente, registrando_equipos, reparando
+from mi_web.views import (registrando_cliente,
+ registrando_equipos, reparando,formulario_reparacion,formulario_proveedores,formulario_repuestos)
 
 urlpatterns = [
    path('clientes/', registrando_cliente),
    path('equipos/', registrando_equipos),
    path('reparaciones/', reparando),
-   
+   path('ordenes',formulario_reparacion),
+   path('proveedores',formulario_proveedores),
+   path('repuestos',formulario_repuestos),
 ]
--- mi_app/mi_web/views.py
+++ mi_app/mi_web/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from mi_web.models import Cliente, Equipo, Reparacion
+from mi_web.forms import Formulario_reparacion, Formulario_proveedores, Formulario_repuestos
 
 def registrando_cliente(request):
     context = {}
@@ -20,3 +21,23 @@ def reparando(request):
     return render(request,'mi_web/reparaciones.html',context)
 
 
+def formulario_reparacion(request):
+    if request.method == "POST":
+        pass
+    else:
+        form_reparacion = Formulario_reparacion()
+        return render (request, "mi_web/ordenes.html",{"form_reparacion":form_reparacion})
+
+def formulario_proveedores(request):
+    if request.method == "POST":
+        pass
+    else:
+        form_proveedor = Formulario_proveedores()
+        return render (request, "mi_web/proveedores.html",{"form_proveedor":form_proveedor})
+
+def formulario_repuestos(request):
+    if request.method == "POST":
+        pass
+    else:
+        form_repuesto = Formulario_repuestos()
+        return render (request, "mi_web/repuestos.html",{"form_repuesto":form_repuesto})

b611ebde35f9f724be4205ce62b56baa90c8ca31
Christian-Coria <desbloqueoenlinea@gmail.com>
2022-07-10 11:06:45 -0300

    forms

---
+++ mi_app/mi_web/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+    
+class Formulario_reparacion(forms.Form):
+
+    nombre = forms.CharField(max_length=40)
+    apellido = forms.CharField(max_length=40)
+    telefono=forms.IntegerField()
+    email=forms.EmailField()
+    equipo = forms.CharField(max_length=40)
+    falla = forms.CharField(max_length=40)
+    
+class Formulario_proveedores(forms.Form):
+    nombre = forms.CharField(max_length=40)
+    telefono = forms.CharField(max_length=40)
+
+class Formulario_repuestos(forms.Form):
+    clase = forms.CharField(max_length=40)
+    marca = forms.CharField(max_length=40)
+    modelo = forms.CharField(max_length=40)
+    precio = forms.IntegerField()
